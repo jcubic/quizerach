af5121bfb4fc6a1e253881af8bc5e4244de1975a
Jakub Jankiewicz <jcubic@onet.pl>
2023-02-27 16:20:27 +0100

    add question using ejs - fix #3

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const session = require('express-session');
 const sqlite3 = require('sqlite3').verbose();
+const { marked } = require('marked');
 
 const send_email = require('./email');
 const { unique_token } = require('./utils');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
 const port = 3000;
 
+/*
 send_email({
     email: 'jcubic@onet.pl',
     subject: 'Hello',
@@ -15,11 +20,37 @@ send_email({
 }).then(res => {
     console.log(res);
 });
+*/
 
-/*
-app.use(express.static('public'));
+const question = `
+przeczytaj kod:
+\`\`\`javascript
+function sum(a, b) {
+    return a + b;
+}
+\`\`\`
+czy powyższa funkcja w języku JavaScript to:
+`;
+
+app.use('/public', express.static('public'));
+
+app.get('/', function(req, res) {
+    res.render('pages/index', {
+        question: marked.parse(question),
+        options: [
+            'Wyrażenie',
+            'Deklaracja',
+            'Równanie'
+        ].map((label, i) => {
+            const char = String.fromCharCode(97 + i);
+            return {
+                icon: `letter_${char}`,
+                label
+            };
+        })
+    });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-*/
