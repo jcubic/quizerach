2bfd291f85bd1fc311cb0b32d629ef607ad5a825
Jakub Jankiewicz <jcubic@onet.pl>
2023-04-03 15:58:16 +0200

    add doc string to User Model

--- quiz/graphql/types.ts
+++ quiz/graphql/types.ts
@@ -12,6 +12,18 @@ export const GQLDate = asNexusMethod(GraphQLDateTime, 'date');
 
 export const User = objectType({
     name: 'User',
+    description: `
+## User
+
+User is object that represent a user that start a quiz.
+The most important thing about User is his email that is
+unique identefier that is used to login and take part in
+a quiz.
+
+The user model also have other data that is in database
+like token and token_expiration time for user to login to the
+take part in a quiz.
+`,
     definition(t) {
         t.nonNull.int('user_id');
         t.nonNull.string('email');
