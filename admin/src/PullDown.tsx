cbe1608ee11d19655c972289b43f3024e33bd16c
Jakub Jankiewicz <jcubic@onet.pl>
2023-04-09 20:13:07 +0200

    wrap pullDown items with flex (admin)

--- admin/src/PullDown.tsx
+++ admin/src/PullDown.tsx
@@ -1,3 +1,5 @@
+import { Flex } from '@chakra-ui/react';
+
 type PullDownT = {
     title: string;
     open?: boolean;
@@ -8,7 +10,9 @@ export default function PullDown({ children, title, open }: PullDownT) {
     return (
         <details style={{width: '100%'}} open={open}>
           <summary>{title}</summary>
-          {children}
+          <Flex flexDir="column" gap={2}>
+            {children}
+          </Flex>
         </details>
     );
 };
